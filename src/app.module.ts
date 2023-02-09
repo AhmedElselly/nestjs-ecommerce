bade17931e088294ef8f424d52b665f964b280d2
ahmedelselly <ahmedelselly@localhost.localdomain>
2023-02-09 15:29:35 +0200

    app.module.ts modified

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,8 @@ import { UsersModule } from './users/users.module';
 import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/ecommerce_nestjs'), PostsModule, UsersModule, CommentsModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://ahmed:ahmed01020597841@cluster0.rumcgmz.mongodb.net/?retryWrites=true&w=majority'), PostsModule, UsersModule, CommentsModule],
+  // imports: [MongooseModule.forRoot('mongodb://localhost:27017/ecommerce_nestjs'), PostsModule, UsersModule, CommentsModule],
   // controllers: [AppController],
   // providers: [AppService],
 })
